4714cb4f04839c26ddb93271d3ec40dd37b4b551
Baizul Islam <baizul.islam@tychotechnologies.in>
2022-06-05 17:06:29 +0530

    coimic added

---
+++ src/components/profile/Comic/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserDetails from './UserDetails';
+
+const Comic = ({}) => {
+    return(
+        <React.Fragment>
+            <div>
+                <UserDetails/>
+            </div>
+        </React.Fragment>
+    )
+}
+export default Comic;
--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -3,22 +3,49 @@ import Theme1 from './theme1';
 import Theme1Bg from './theme1/theme1WithBg';
 import Theme2 from './theme2';
 import Theme2Bg from './theme2/theme2WithBg';
+import Comic from './Comic';
 
 const Theme = ({theme, id, data, iurl, ...props}) => {
-    if(theme == "Light" || theme == "White") {
-        if(data.BackgroundImage) {
-            return (<Theme1Bg data={data} id={id}/>)
-        } else {
-            return(<Theme1 data={data} id={id}/>)
-        } 
-    } 
-    else if (theme == "Dark") {
-        if(data.BackgroundImage) {
-            return(<Theme2Bg data={data} id={id} />)
-        } else {
-            return(<Theme2 data={data} id={id}/>)
-        } 
+    // theme = "Comic"
+    switch(theme){
+        case "Light":
+        case "White":
+            if(data.BackgroundImage) {
+                return (<Theme1Bg data={data} id={id}/>)
+            } else {
+                return(<Theme1 data={data} id={id}/>)
+            } 
+        
+        case "Dark":
+            if(data.BackgroundImage) {
+                return(<Theme2Bg data={data} id={id} />)
+            } else {
+                return(<Theme2 data={data} id={id}/>)
+            } 
+        
+        case "Comic":
+            return <Comic/>
+
+        default:
+            <React.Fragment></React.Fragment>
         
     }
+
+
+    // if(theme == "Light" || theme == "White") {
+    //     if(data.BackgroundImage) {
+    //         return (<Theme1Bg data={data} id={id}/>)
+    //     } else {
+    //         return(<Theme1 data={data} id={id}/>)
+    //     } 
+    // } 
+    // else if (theme == "Dark") {
+    //     if(data.BackgroundImage) {
+    //         return(<Theme2Bg data={data} id={id} />)
+    //     } else {
+    //         return(<Theme2 data={data} id={id}/>)
+    //     } 
+        
+    // }
 }
 export default Theme;
